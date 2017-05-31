1121ee0ae849dff1c4ddc774dd38b1d3f69c5c94
corneliusoficu <corneliu.soficu@gmail.com>
2017-05-31 16:20:22 +0300

    Fixed docker bug.

--- src/main/java/com/hazardmanager/users/services/UserService.java
+++ src/main/java/com/hazardmanager/users/services/UserService.java
@@ -3,4 +3,5 @@ package com.hazardmanager.users.services;
 import com.hazardmanager.users.models.User;
 
 public interface UserService extends CrudService<User> {
+ String getUserId(String username, String password);
 }
